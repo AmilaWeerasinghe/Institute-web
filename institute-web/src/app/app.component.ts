ba18298756708450e81abfb5e5d390d29ce47ea3
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2019-04-20 22:23:31 +0530

    home page impkementing

--- institute-web/src/app/app.component.ts
+++ institute-web/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sathma';
+  username='';
+
+  onLogoClick(){
+    alert('Logo clicked');
+  }
+
+  OnkeyUp(newTitle:string){
+    this.username=newTitle;
+  }
+
+  
 }
